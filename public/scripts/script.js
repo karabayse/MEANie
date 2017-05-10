673a6ec94bc06c087dadbc0900d3c904cfa885c3
Kara Bayse <karabayse@gmail.com>
2017-05-10 16:56:57 -0500

    fixed formatting and bugs fixed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,49 @@
-var myApp=angular.module( 'myApp', [] );
-myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
+// check line 10 of index.html --> make sure 'myApp' matches
+var myApp = angular.module( 'myApp', [] );
+
+// set up a controller
+myApp.controller( 'WhereMyPeeps', function( $http ){
+
+// vriable global to this controller
 var vm = this;
+
+// array attached to controller (makes it available to DOM)
+vm.records = [];
+
+// "vm" stands for "view model"
 vm.addRecord = function(){
+  console.log('in addItem ng-click');
 var objectToSend ={
-name= vm.nameIn,
-location= vm.locationIn,
+  name: vm.nameIn,
+  location: vm.locationIn
 };
+  console.log('adding objectToSend ->', objectToSend);
+
+// angular post call
 $http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
+  method: 'POST',
+  url: '/getRecords',
+  data: objectToSend
+}).then(function(response){
+  console.log('back from server ->', response);
+  vm.getRecords();
+}); // end http
+
+// empty inputs
 vm.nameIn ='';
-vm.locationIn='';
-};
+vm.locationIn ='';
+// update from server
+}; // end add item
+
 vm.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-vm.allTheRecords = response;
-console.log( vm.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
+  console.log('in getRecords');
+  $http({
+  method: 'GET',
+  url: '/getRecords',
+}).then( function success( response ){
+  console.log('response ->', response);
+  vm.records = response.data;
+  console.log( vm.records );
+});
 };
-};
-}]);
+});
--- server/app.js
+++ server/app.js
@@ -1,38 +1,70 @@
+// requires
+// node modules
 var express = require( 'express' );
-var app=express();
+var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
+var mongoose = require('mongoose');
+
+// uses
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use( bodyParser.json() );
-var mongoose = require( 'mongoose' );
+
 // 27017 is default mongo port
 mongoose.connect( 'localhost:27017/meanie' );
-var ourSchema = new  mongoose.Schema({
-name: String,
-location: String
+
+// create Schema
+var ourSchema = mongoose.Schema({
+  name: String,
+  location: String
 });
-var ourModel = mongoose.model( 'ourModel', ourSchema );
+
+// global variable
+var port = process.env.Port || 8080;
+
+// asking mongoose to take our Schema and make a collection
+var ourModels = mongoose.model( 'ourModels', ourSchema );
+
+// get route for html
 app.get( '/', function( req, res ){
-res.sendFile( path.resolve( 'public/index.html' ) );
+  console.log('base url hit', path.resolve ('public/index.html'));
+  res.sendFile( path.resolve( 'public/index.html' ) );
 });
+
 app.get( '/getRecords', function( req, res ){
-// get and send back all the things
-ourModel.find().then( function( data ){
-res.send( data );
-});
-});
-app.listen( 8080, 'localhost', function( req, res ){
-console.log( 'listening on 8080' );
-});
-app.post( '/testPost', function( req, res ){
-console.log( 'req.body.name: ' + req.body.name );
+    console.log('get records');
+    // get and send back all the things
+    ourModels.find().then( function( data ){
+      res.send( data );
+    }); // end get to find records
+}); // end server get to get records
+
+//
+app.post( '/getRecords', function( req, res ){
+  console.log( 'req.body.name: ' + req.body.name );
 // retrieved the req.body
+
 // putting it into an object to be saved in the db
-var recordToAdd={
-name:req.body.name,
-location:req.body.location
-}
+var recordToAdd = {
+  name:req.body.name,
+  location:req.body.location
+};
+
+// recordToAdd.save(function(err){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log('recordToAdd created');
+//     res.sendStatus(200);
+//   }
+// });
+
 // create new record
-var newRecord=ourModel( recordToAdd );
-newRecord.save();
+var newRecord = ourModels( recordToAdd );
+  newRecord.save();
 });
-app.use( express.static( 'public' ) );
+
+app.listen(port, function(){
+  console.log( 'listening on 8080 ->', port);
+});
